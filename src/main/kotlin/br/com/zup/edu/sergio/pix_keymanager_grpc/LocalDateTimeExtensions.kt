70fe7f71f440b69bf8d843776ec3506dc1ba2e15
sergio <sergio.amorim@zup.com.br>
2021-08-28 20:33:45 -0300

    fix conversion of LocalDateTime to Timestamp

--- src/main/kotlin/br/com/zup/edu/sergio/pix_keymanager_grpc/LocalDateTimeExtensions.kt
+++ src/main/kotlin/br/com/zup/edu/sergio/pix_keymanager_grpc/LocalDateTimeExtensions.kt
@@ -2,6 +2,11 @@ package br.com.zup.edu.sergio.pix_keymanager_grpc
 
 import com.google.protobuf.Timestamp
 import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 val LocalDateTime.googleProtobufTimestamp: Timestamp
-  get() = Timestamp.newBuilder().setNanos(this.nano).build()
+  get() = Timestamp
+    .newBuilder()
+    .setSeconds(this.toEpochSecond(ZoneOffset.UTC))
+    .setNanos(this.nano)
+    .build()
